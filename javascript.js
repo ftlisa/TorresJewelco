6d828d305119f38959cdaececf4a8d635934af0c
Lisa Muvirimi <lmuvirimi@student.unimelb.edu.au>
2025-06-06 13:03:15 +1000

    adding slide in animation

--- javascript.js
+++ javascript.js
@@ -8,3 +8,18 @@ hamburger.addEventListener('click', () => {
   // Toggle hamburger ↔ X
   hamburger.innerHTML = hamburger.classList.contains('open') ? '&times;' : '&#9776;';
 });
+
+const observers = document.querySelectorAll('.slide-in-left, .slide-in-right');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+        observer.unobserve(entry.target); // stops watching after it appears
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  observers.forEach(el => observer.observe(el));
